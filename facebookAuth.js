04fbc6f9d06dedeb666b10e98a227c83c549296f
Arjun Wadhwa <arjunw7@gmail.com>
2016-07-10 22:40:52 +0530

    facebook auth update

--- facebookAuth.js
+++ facebookAuth.js
@@ -2,6 +2,6 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID': '749227148552853',
 		'clientSecret': 'fb653b8a1bab4b5e8d8ce3931ea1a68f',
-		'callbackURL': 'http://sasitravels.herokuapp.com/auth/facebook/callback'
+		'callbackURL': 'http://sasitravels.in/auth/facebook/callback'
 	}
 }
