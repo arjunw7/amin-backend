17993643321d75df451c6ea8fbd42c83c4b41c91
Arjun Wadhwa <arjunw7@gmail.com>
2016-07-10 08:51:10 +0530

    config error rectified

--- facebookAuth.js
+++ facebookAuth.js
@@ -2,6 +2,6 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID': '749227148552853',
 		'clientSecret': 'fb653b8a1bab4b5e8d8ce3931ea1a68f',
-		'callbackURL': 'http://localhost:3000/auth/facebook/callback'
+		'callbackURL': 'http://sasitravels.herokuapp.com/auth/facebook/callback'
 	}
 }
